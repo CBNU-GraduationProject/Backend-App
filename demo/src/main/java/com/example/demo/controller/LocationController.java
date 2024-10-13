2a149711a51f75e33ff01a72b0dfbf144fa1b37c
minjae <robot703@naver.com>
2024-10-13 20:20:51 +0900

    add repository/LocationRepository

--- demo/src/main/java/com/example/demo/controller/LocationController.java
+++ demo/src/main/java/com/example/demo/controller/LocationController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Location;
-import com.example.demo.repository.LocationRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
